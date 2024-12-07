b3fd650d3efd888c178297d6b2ccd758b7b9ef1a
adisovaa <79251934+adisovaa@users.noreply.github.com>
2024-12-07 00:42:56 -0800

    localStorage

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+
+    const [value, setValue] = useState(0)
+
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+        localStorage.setItem('counterValue + 1', JSON.stringify(value))
+    }
+    const getToLocalStorageHandler = () => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }
+    const clearLocalStorageHandler = () => {
+        localStorage.clear()
+        setValue(0)
+    }
+
+    const removeLocalStorageHandler = () => {
+        localStorage.removeItem('counterValue + 1')
+    }
+
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+            <button onClick={getToLocalStorageHandler}>getFromLocalStorage</button>
+            <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
+            <button onClick={removeLocalStorageHandler}>removeLocalStorage</button>
+        </div>
+    );
+}
+
+export default App;
